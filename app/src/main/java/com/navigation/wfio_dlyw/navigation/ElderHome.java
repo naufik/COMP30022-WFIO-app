c81fda6ca0da479380eb9f748b968b2e1372c577
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-08-13 16:45:50 +1000

    elderHome and elderNavigation is now connected

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderHome.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderHome.java
@@ -1,7 +1,11 @@
 package com.navigation.wfio_dlyw.navigation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class ElderHome extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class ElderHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elder_home);
+        final Button navigationButton = (Button)findViewById(R.id.navigateButton);
+        navigationButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent startIntent = new Intent(getApplicationContext(), ElderNavigation.class);
+                startIntent.putExtra("test", "Hello World?");
+                startActivity(startIntent);
+            }
+        });
     }
+
 }
