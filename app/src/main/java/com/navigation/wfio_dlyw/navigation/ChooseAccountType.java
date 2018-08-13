cf0fcb7f83c1d5d58669478d220fa14c6b68e230
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-08-14 00:28:15 +1000

    added login, signup, and chooseaccounttype activities
    1st draft design

---
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/ChooseAccountType.java
@@ -0,0 +1,13 @@
+package com.navigation.wfio_dlyw.navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseAccountType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
