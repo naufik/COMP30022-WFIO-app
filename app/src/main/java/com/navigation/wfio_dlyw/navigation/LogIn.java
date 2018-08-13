29fb0b77db5f4d7902dde9ce6a9d3a1937e28c18
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-08-14 00:34:12 +1000

    added navigation to login page

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/LogIn.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/LogIn.java
@@ -1,7 +1,9 @@
 package com.navigation.wfio_dlyw.navigation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
@@ -10,5 +12,22 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        //for the moment it only redirects to the elder's home page
+        Button enterBtn = (Button)findViewById(R.id.enterBtn);
+        enterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), ElderHome.class);
+                startActivity(startIntent);
+            }
+        });
+        final Button signUpBtn = (Button)findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
