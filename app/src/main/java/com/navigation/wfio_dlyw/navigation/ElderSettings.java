738da22e29c590eba2f811b69d3425600b62a044
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-08-14 12:35:56 +1000

    moved logout button to settings

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderSettings.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderSettings.java
@@ -1,7 +1,10 @@
 package com.navigation.wfio_dlyw.navigation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ElderSettings extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class ElderSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elder_settings);
+        Button elderLogOutBtn = (Button)findViewById(R.id.logoutBtn);
+        elderLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
