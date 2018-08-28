f09707ea9be01e056d01576f5d49d2c5f44d59ef
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-08-28 13:17:14 +1000

    added new menu resource directory and added arrow.png

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/CarerHome.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/CarerHome.java
@@ -6,11 +6,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ToolbarWidgetWrapper;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.support.v7.widget.Toolbar;
 
-public class CarerHome extends AppCompatActivity {
+public class CarerHome extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,4 +38,26 @@ public class CarerHome extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.tool, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle presses on the action bar items
+//        switch (item.getItemId()) {
+//            case R.id.action_search:
+//                openSearch();
+//                return true;
+//            case R.id.action_compose:
+//                composeMessage();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 }
