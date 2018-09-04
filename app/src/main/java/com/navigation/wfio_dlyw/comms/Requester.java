da5d780c184142e581c25383c9a245312dc1c683
Naufal Fikri <nauffik@gmail.com>
2018-09-04 13:14:33 +1000

    Added request handler for GET and POST requests to the server

--- app/src/main/java/com/navigation/wfio_dlyw/comms/Requester.java
+++ app/src/main/java/com/navigation/wfio_dlyw/comms/Requester.java
@@ -1,14 +1,20 @@
 package com.navigation.wfio_dlyw.comms;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Requester {
 
-    public static String SERVER_URL = "http://rawon.naufik.net";
+    public static String SERVER_URL = "http://rawon.naufik.net:3000";
 
     private static Requester instance;
     private Context context;
@@ -29,6 +35,28 @@ public class Requester {
     }
 
     public <T> void addRequest(Request<T> req) {
+        this.requestQueue.add(req);
+    }
+
+    public void POSTRequest(@Nullable JSONObject body, Response.Listener<JSONObject> onResponse) {
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, SERVER_URL,
+                body, onResponse, new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError err) {
+
+                    }
+                });
+        this.requestQueue.add(req);
+    }
+
+    public void GETRequest(@Nullable JSONObject body, Response.Listener<JSONObject> onResponse) {
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, SERVER_URL,
+                body, onResponse, new Response.ErrorListener(){
+                @Override
+                public void onErrorResponse(VolleyError err) {
+
+                }
+        });
         this.requestQueue.add(req);
     }
 
