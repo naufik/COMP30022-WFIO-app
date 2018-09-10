97a810e38beb24a036f827da367d880e43596fbb
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-09-10 15:42:50 +1000

    added notification feature

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderNavigation.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderNavigation.java
@@ -3,8 +3,12 @@ package com.navigation.wfio_dlyw.navigation;
 import com.android.volley.Response;
 import com.navigation.wfio_dlyw.comms.*;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -23,10 +27,25 @@ import java.util.concurrent.locks.ReadWriteLock;
 
 public class ElderNavigation extends AppCompatActivity {
 
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 45612;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elder_navigation);
+
+        notification  = new NotificationCompat.Builder(ElderNavigation.this, "45612");
+        notification.setAutoCancel(true);
+
+        Button notif = (Button) findViewById(R.id.notif);
+        notif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonClicked(view);
+            }
+        });
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbarEN);
         setSupportActionBar(myToolbar);
@@ -38,6 +57,24 @@ public class ElderNavigation extends AppCompatActivity {
                 exampleRequest();
             }
         });
+
+    }
+
+    public void ButtonClicked(View view) {
+        //Build the notification
+        notification.setSmallIcon(R.drawable.ic_launcher_background);
+        notification.setTicker("This is the ticker");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Here is the title");
+        notification.setContentText("I am body test of your notification");
+
+        Intent intent = new Intent(this, ElderNavigation.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Build Notification and issues it
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(uniqueID, notification.build());
     }
 
     private void exampleRequest(){
