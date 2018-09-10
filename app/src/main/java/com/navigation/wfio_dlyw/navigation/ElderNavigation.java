da26286bc8318de0154e2e3db560243a29204867
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-09-10 19:13:13 +1000

    fixed notification channels

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderNavigation.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderNavigation.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,6 +18,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,59 +28,34 @@ import org.json.JSONObject;
 import java.util.concurrent.locks.ReadWriteLock;
 
 public class ElderNavigation extends AppCompatActivity {
-
+    private NotificationManagerCompat notificationManager;
     public static final String channel_1_ID = "channel 1";
-    public static final String channel_2_ID = "channel 2";
-    private static final int uniqueID = 45612;
-
-    private void createNotificationChannels(){
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            NotificationChannel channel1= new NotificationChannel(
-                    channel_1_ID,
-                    "channel 1",
-                    NotificationManager.IMPORTANCE_HIGH
-            );
-            channel1.setDescription("This is Channel 1");
-            NotificationChannel channel2= new NotificationChannel(
-                    channel_2_ID,
-                    "channel 1",
-                    NotificationManager.IMPORTANCE_LOW
-            );
-            channel2.setDescription("This is Channel 2");
-
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(channel1);
-            manager.createNotificationChannel(channel2);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elder_navigation);
-
-//        Button notif = (Button) findViewById(R.id.notif);
-//        notif.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                ButtonClicked(view);
-//            }
-//        });
+        createNotificationChannels();
+        notificationManager = NotificationManagerCompat.from(this);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbarEN);
         setSupportActionBar(myToolbar);
+    }
 
-//        Button favoritesButton = (Button) findViewById(R.id.favoritesButton);
-//        favoritesButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                exampleRequest();
-//            }
-//        });
+    public void sendOnChannel(View v){
+        String title = "New Notif";
+        String message = "yeet";
+
+        Notification notification = new NotificationCompat.Builder(this, channel_1_ID)
+                .setSmallIcon(R.drawable.ic_child)
+                .setContentTitle(title)
+                .setContentText(message)
+                .build();
+        //need to give different id's if you want to give multiple notifications instanteneously
+        notificationManager.notify(1, notification);
 
     }
 
-//
 //    private void exampleRequest(){
 //        Requester rs = Requester.getInstance(this.getApplicationContext());
 //
@@ -98,6 +75,21 @@ public class ElderNavigation extends AppCompatActivity {
 //    private void makeToast(String msg) {
 //        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
 //    }
+    public void createNotificationChannels(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel1= new NotificationChannel(
+                    channel_1_ID,
+                    "channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("This is Channel 1");
+            channel1.enableVibration(true);
+            channel1.enableLights(true);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+    }
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
