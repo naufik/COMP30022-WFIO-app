27acb89538723f7c84d6a2ca1d38e7802b367067
Naufal Fikri <nauffik@gmail.com>
2018-09-10 16:13:58 +1000

    added server status method to... check for server status

--- app/src/main/java/com/navigation/wfio_dlyw/comms/Requester.java
+++ app/src/main/java/com/navigation/wfio_dlyw/comms/Requester.java
@@ -8,11 +8,14 @@ import com.android.volley.*;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.function.Function;
 
 public class Requester {
 
-    public static String SERVER_URL = "http://rawon.naufik.net:3000";
+    public static String SERVER_URL = "http://rawon.naufik.net:3000/";
 
     private static Requester instance;
     private Context context;
@@ -22,7 +25,6 @@ public class Requester {
     private Requester(Context context) {
         this.context = context;
         this.requestQueue = Volley.newRequestQueue(context.getApplicationContext());
-        this.requestQueue.start();
     }
 
     public static synchronized Requester getInstance(Context context) {
@@ -37,28 +39,55 @@ public class Requester {
         this.requestQueue.add(req);
     }
 
-    public void POSTRequest(@Nullable JSONObject body, Response.Listener<JSONObject> onResponse) {
-        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, SERVER_URL,
-                body, onResponse, new Response.ErrorListener(){
+    public void POSTRequest(String endpoint, @Nullable JSONObject body, Response.Listener<JSONObject> onResponse) {
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST,
+                SERVER_URL + endpoint,
+                body,
+                onResponse,
+                new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError err) {
-                        Toast.makeText(context.getApplicationContext(), err.getMessage(),
+                        Toast.makeText(context, err.getMessage(),
                                 Toast.LENGTH_LONG);
                     }
                 });
-        this.requestQueue.add(req);
+        this.addRequest(req);
     }
 
-    public void GETRequest(Response.Listener<JSONObject> onResponse) {
-        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, SERVER_URL,
-                null, onResponse, new Response.ErrorListener(){
+    public void GETRequest(String endpoint, Response.Listener<JSONObject> onResponse) {
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET,
+                SERVER_URL + endpoint,
+                null,
+                onResponse,
+                new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError err) {
-                        Toast.makeText(context.getApplicationContext(), err.getMessage(),
+                        Toast.makeText(context, err.getMessage(),
                                 Toast.LENGTH_LONG);
                     }
         });
-        this.requestQueue.add(req);
+        this.addRequest(req);
     }
 
+    public void getServerStatus(Function<Boolean, Void> onStatusReceived) {
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET,
+                SERVER_URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            onStatusReceived.apply((Boolean) response.get("status"));
+                        } catch (JSONException e) {
+
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError e){
+
+                    }
+                });
+    }
 }
