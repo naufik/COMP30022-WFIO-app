eed4c528de43054622347d384b70f6e1ef7650fd
Surviantoro Ilham Yudanardi <syudanardi@student.unimelb.edu.au>
2018-09-10 21:22:49 +1000

    Declining in notification page brings to login

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/AnswerHelp.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/AnswerHelp.java
@@ -1,7 +1,9 @@
 package com.navigation.wfio_dlyw.navigation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AnswerHelp extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class AnswerHelp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_help);
+
+        Button decline = (Button)findViewById(R.id.decline);
+        decline.setOnClickListener(view -> {
+            Intent startIntent = new Intent(getApplicationContext(), LogIn.class);
+            startActivity(startIntent);
+        });
     }
 }
