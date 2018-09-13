fbb9f9e2b9b0d72d0571af4ac4f3925d2312787b
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-09-13 17:26:54 +1000

    added edittext conditions for newpassword and currentpassword pages

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/EnterCurrentPassword.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/EnterCurrentPassword.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EnterCurrentPassword extends AppCompatActivity {
 
@@ -13,12 +15,22 @@ public class EnterCurrentPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_current_password);
 
+
+
         Button currentPasswordBtn = (Button)findViewById(R.id.currentPasswordBtn);
         currentPasswordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), NewPassword.class);
-                startActivity(startIntent);
+                EditText currentPassword = (EditText)findViewById(R.id.currentPassword);
+                String currentPasswordS = currentPassword.getText().toString();
+                if(currentPasswordS.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Please enter your current " +
+                            "password", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Intent startIntent = new Intent(getApplicationContext(), NewPassword.class);
+                    startActivity(startIntent);
+                }
             }
         });
     }
--- app/src/main/java/com/navigation/wfio_dlyw/navigation/NewPassword.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/NewPassword.java
@@ -15,23 +15,26 @@ public class NewPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_password);
 
-        EditText newPassword = (EditText) findViewById(R.id.newPassword);
-        EditText retypeNewPassword = (EditText) findViewById(R.id.retypeNewPassword);
-
-        String newPasswordS = newPassword.getText().toString();
-        String retypeNewPasswordS = retypeNewPassword.getText().toString();
-
-
         Button newPasswordBtn = (Button)findViewById(R.id.newPasswordBtn);
         newPasswordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText newPassword = (EditText) findViewById(R.id.newPassword);
+                EditText retypeNewPassword = (EditText) findViewById(R.id.retypeNewPassword);
+
+                String newPasswordS = newPassword.getText().toString();
+                String retypeNewPasswordS = retypeNewPassword.getText().toString();
+
                 //as of now it goes back to elder settings even if your originally login as a carer
-                if(newPasswordS.equals(retypeNewPasswordS)){
+                if(newPasswordS.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter a new password", Toast.LENGTH_LONG).show();
+                }else if(retypeNewPasswordS.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Please Re-Type your new Password", Toast.LENGTH_LONG).show();
+                }else if(!newPasswordS.equals(retypeNewPasswordS)){
+                    Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_LONG).show();
+                }else{
                     Intent startIntent = new Intent(getApplicationContext(), ElderSettings.class);
                     startActivity(startIntent);
-                }else{
-                    Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_LONG).show();
                 }
             }
         });
