b8d4db19926e57cd669ed1358183aefb03a8d72b
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-09-18 01:44:26 +1000

    added ability to remove/insert elders from myelders

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/MyElders.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/MyElders.java
@@ -4,24 +4,75 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class MyElders extends AppCompatActivity {
+    private ArrayList<ElderItem> elders;
+
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    private Button buttonInsert;
+    private Button buttonRemove;
+    private EditText editTextInsert;
+    private EditText editTextRemove;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_elders);
 
-        ArrayList<ElderItem> elders = new ArrayList<>();
+        createElders();
+        buildRecyclerViewer();
+
+        buttonInsert = findViewById(R.id.button_insert);
+        buttonRemove = findViewById(R.id.button_remove);
+        editTextInsert = findViewById(R.id.edittext_insert);
+        editTextRemove = findViewById(R.id.edittext_remove);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
+
+
+
+
+    }
+
+    public void insertItem(int position){
+        elders.add(position, new ElderItem("New Item At Position"+position, "This is Line 2"));
+        mAdapter.notifyItemInserted(position);
+    }
+
+    public void removeItem(int position){
+        elders.remove(position);
+        mAdapter.notifyItemRemoved(position);
+    }
+
+    public void createElders(){
+        elders = new ArrayList<>();
         elders.add(new ElderItem("stacy", "stacy"));
         elders.add(new ElderItem("Damn", "stacy"));
         elders.add(new ElderItem("John", "stacy"));
+    }
 
+    public void buildRecyclerViewer(){
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
@@ -30,5 +81,7 @@ public class MyElders extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
     }
+
+
 
 }
