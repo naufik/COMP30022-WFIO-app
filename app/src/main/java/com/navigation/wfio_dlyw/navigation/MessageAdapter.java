13e353d7d98cee1324bc30e1ad1a8a4277d16d29
StaticDDQ <31177227+StaticDDQ@users.noreply.github.com>
2018-09-18 03:42:13 +1000

    added list of voice clips in MessageList

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/MessageAdapter.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/MessageAdapter.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.media.MediaPlayer;
+import android.speech.tts.Voice;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +14,15 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MessageAdapter extends BaseAdapter {
+public class MessageAdapter extends BaseAdapter{
 
-    List<Message> messages = new ArrayList<Message>();
-    Context context;
+    private MediaPlayer mPlayer = null;
+    private List<Message> messages = new ArrayList<Message>();
+    private Context context;
 
     public MessageAdapter(Context context) {
         this.context = context;
@@ -48,41 +53,47 @@ public class MessageAdapter extends BaseAdapter {
     public View getView(int i, View convertView, ViewGroup viewGroup) {
         Message message = messages.get(i);
         LayoutInflater messageInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        if(message instanceof VoiceClip){
-            ClipViewHolder holder = new ClipViewHolder();
+        MessageViewHolder holder = new MessageViewHolder();
 
-            if (message.isBelongsToCurrentUser()) {
-                convertView = messageInflater.inflate(R.layout.my_clip, null);
-                holder.clip = (Button) convertView.findViewById(R.id.message_body);
-                convertView.setTag(holder);
-                holder.clip = ((VoiceClip) message).getClip();
-            } else{
-                convertView = messageInflater.inflate(R.layout.their_clip, null);
-                holder.clip = (Button) convertView.findViewById(R.id.message_body);
-                convertView.setTag(holder);
-                holder.clip = ((VoiceClip) message).getClip();
-            }
-        } else{
-            MessageViewHolder holder = new MessageViewHolder();
+        if (message.isBelongsToCurrentUser()) { // this message was sent by us so let's create a basic chat bubble on the right
+            convertView = messageInflater.inflate(R.layout.my_message, null);
+            holder.messageBody = (TextView) convertView.findViewById(R.id.message_body);
+            convertView.setTag(holder);
+            holder.messageBody.setText(message.getText());
+        }else{ // this message was sent by someone else so let's create an advanced chat bubble on the left
+            convertView = messageInflater.inflate(R.layout.their_message, null);
+            holder.messageBody = (TextView) convertView.findViewById(R.id.message_body);
+            holder.name = (TextView) convertView.findViewById(R.id.name);
+            holder.name.setText(message.getUsername());
+            convertView.setTag(holder);
+            holder.messageBody.setText(message.getText());
+            //set the username of the sender here
 
-            if (message.isBelongsToCurrentUser()) { // this message was sent by us so let's create a basic chat bubble on the right
-                convertView = messageInflater.inflate(R.layout.my_message, null);
-                holder.messageBody = (TextView) convertView.findViewById(R.id.message_body);
-                convertView.setTag(holder);
-                holder.messageBody.setText(message.getText());
-            }else{ // this message was sent by someone else so let's create an advanced chat bubble on the left
-                convertView = messageInflater.inflate(R.layout.their_message, null);
-                holder.messageBody = (TextView) convertView.findViewById(R.id.message_body);
-                holder.name = (TextView) convertView.findViewById(R.id.name);
-                holder.name.setText(message.getUsername());
-                convertView.setTag(holder);
-                holder.messageBody.setText(message.getText());
-                //set the username of the sender here
-            }
         }
+
+
         return convertView;
     }
 
+    private void startPlaying(String file) {
+
+        if(mPlayer != null && mPlayer.isPlaying())
+            stopPlaying();
+
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(file);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
 }
 
 class MessageViewHolder {
@@ -91,6 +102,3 @@ class MessageViewHolder {
     public TextView messageBody;
 }
 
-class ClipViewHolder {
-    public Button clip;
-}
