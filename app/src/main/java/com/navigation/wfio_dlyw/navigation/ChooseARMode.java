e6c7e0d763e8512f229f3ebce7f9f2a7854e5556
StaticDDQ <31177227+StaticDDQ@users.noreply.github.com>
2018-09-17 18:32:05 +1000

    added another activity to choose either ar mode or map view, removed unused imports

---
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/ChooseARMode.java
@@ -0,0 +1,6 @@
+package com.navigation.wfio_dlyw.navigation;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ChooseARMode extends AppCompatActivity {
+}
--- app/src/main/java/com/navigation/wfio_dlyw/navigation/RecordVoice.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/RecordVoice.java
@@ -8,13 +8,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import java.io.File;
