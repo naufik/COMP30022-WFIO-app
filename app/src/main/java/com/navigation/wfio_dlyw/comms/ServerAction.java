94efcb265e12d89392b9aa1808b2bae08b7158e9
Naufal Fikri <nauffik@gmail.com>
2018-09-29 18:42:36 +1000

    removed deprecated method to get the user favorite places

--- app/src/main/java/com/navigation/wfio_dlyw/comms/ServerAction.java
+++ app/src/main/java/com/navigation/wfio_dlyw/comms/ServerAction.java
@@ -8,11 +8,6 @@ public enum ServerAction {
     USER_SIGN_UP("user.signup", Request.Method.POST),
     USER_GET_INFO("user.details", Request.Method.GET),
     USER_MODIFY_RECORD("user.modify", Request.Method.POST),
-    /**
-     * @deprecated Query the entire user data using "user.details" instead and then grab the
-     * "favorites" field.
-     */
-    @Deprecated  USER_GET_FAVORITES("user.getfavs", Request.Method.GET),
     ELDER_REQUEST_LINK("user.genlink", Request.Method.POST),
     CARER_LINK("user.link", Request.Method.POST),
     MESSAGE_PULL("msg.pull", Request.Method.GET),
