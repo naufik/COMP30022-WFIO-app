e790ab15b97a9a79ad91d9b778088af7ce65f400
Naufal Fikri <nauffik@gmail.com>
2018-09-30 19:31:07 +1000

    Notifications that an elder requires help is now redirected properly with the proper Intent

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/AnswerHelp.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/AnswerHelp.java
@@ -1,9 +1,12 @@
 package com.navigation.wfio_dlyw.navigation;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AnswerHelp extends AppCompatActivity {
 
@@ -12,7 +15,16 @@ public class AnswerHelp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_help);
 
-        Button decline = (Button)findViewById(R.id.decline);
+        TextView text = findViewById(R.id.textView5);
+        text.setText(getIntent().getStringExtra("fromName") + " needs help navigating!");
+
+        Button accept = findViewById(R.id.acceptbutton);
+        accept.setOnClickListener(view -> {
+
+
+        });
+
+        Button decline = findViewById(R.id.decline);
         decline.setOnClickListener(view -> {
             Intent startIntent = new Intent(getApplicationContext(), CarerHome.class);
             startActivity(startIntent);
