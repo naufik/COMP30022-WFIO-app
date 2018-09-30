19a0f086ba1db5db9667cbec71bad5d6775609dd
Naufal Fikri <nauffik@gmail.com>
2018-09-30 20:20:57 +1000

    Added a service to notify elders that a carer has already accepted their help

---
+++ app/src/main/java/com/navigation/wfio_dlyw/comms/NotifyService.java
@@ -0,0 +1,24 @@
+package com.navigation.wfio_dlyw.comms;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+/**
+ * The job of this service is to notify the other party that one has agreed to assist them.
+ */
+public class NotifyService extends IntentService {
+    public NotifyService() {
+        super("NotifyService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Requester.getInstance(this).requestAction(ServerAction.CARER_ACCEPT, null,
+                t -> {
+                    // to be implemented later.
+                },
+                new Credentials(Token.getInstance().getEmail(), Token.getInstance().getValue())
+        );
+    }
+}
