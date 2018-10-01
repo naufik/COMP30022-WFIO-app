7e06a6e2984b3463af1543eb5220144c64d6dd8e
Farhan Darma <fdarma@student.unimelb.edu.au>
2018-10-01 23:09:45 +1000

    imported toast

--- app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderSettings.java
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/ElderSettings.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.navigation.wfio_dlyw.comms.Credentials;
 import com.navigation.wfio_dlyw.comms.Requester;
