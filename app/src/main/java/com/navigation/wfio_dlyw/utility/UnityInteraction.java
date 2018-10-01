996dfd0bdc17874d6ba15e8457ecba7af96a4b60
StaticDDQ <31177227+StaticDDQ@users.noreply.github.com>
2018-10-01 22:30:59 +1000

    updated send help method in UnityInteraction

--- app/src/main/java/com/navigation/wfio_dlyw/utility/UnityInteraction.java
+++ app/src/main/java/com/navigation/wfio_dlyw/utility/UnityInteraction.java
@@ -3,6 +3,10 @@ package com.navigation.wfio_dlyw.utility;
 import android.app.Activity;
 import android.content.Intent;
 
+import com.navigation.wfio_dlyw.comms.Credentials;
+import com.navigation.wfio_dlyw.comms.Requester;
+import com.navigation.wfio_dlyw.comms.ServerAction;
+import com.navigation.wfio_dlyw.comms.Token;
 import com.navigation.wfio_dlyw.navigation.ElderMaps;
 import com.navigation.wfio_dlyw.navigation.ElderNavigation;
 
@@ -19,6 +23,8 @@ public class UnityInteraction {
     }
 
     public static void sendHelp(Activity activity){
-
+        Requester minta = Requester.getInstance(activity.getApplicationContext());
+        Token var = Token.getInstance();
+        minta.requestAction(ServerAction.CARER_SIGNAL, null, response -> {}, new Credentials(var.getEmail(), var.getValue()));
     }
 }
