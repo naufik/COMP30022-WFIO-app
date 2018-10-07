6380506b586e5cb36c0cbc5de65539099148c61b
Naufal Fikri <nauffik@gmail.com>
2018-10-07 19:55:40 +1100

    Added service for initialization when login finishes

---
+++ app/src/main/java/com/navigation/wfio_dlyw/navigation/InitializeService.java
@@ -0,0 +1,20 @@
+package com.navigation.wfio_dlyw.navigation;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.support.annotation.Nullable;
+
+public class InitializeService extends IntentService {
+
+    public InitializeService() {
+        super( "InitializeService" );
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        AudioManager audioMgr = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioMgr.setSpeakerphoneOn(true);
+    }
+}
